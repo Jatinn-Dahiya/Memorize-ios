5d5513b507802ccf13c9c7d15f96b8ad08f45dee
Jatinn-Dahiya <Jatinndahiya@icloud.com>
2025-01-21 16:46:54 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jatinn Dahiya on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
