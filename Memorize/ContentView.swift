d65fddd815056380f303498cf30f4494e1df45b6
Jatinn-Dahiya <Jatinndahiya@icloud.com>
2025-01-21 16:55:00 +0530

    Test

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -25,7 +25,7 @@ struct CardView: View {
     var body: some View {
         ZStack(content: {
             if isFaceUp {
-                RoundedRectangle (cornerRadius: 12) .foregroundColor (.white)
+                RoundedRectangle (cornerRadius: 14) .foregroundColor (.white)
                 RoundedRectangle (cornerRadius: 12) .strokeBorder(lineWidth: 2)
                 Text ("👻").font(.largeTitle)
             } else {
